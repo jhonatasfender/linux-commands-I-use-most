52798ba580a3aa688620031938d1af87054d3b3e
jhonatasfender <jhonatas.fender@gmail.com>
2016-10-03 07:29:06 -0300

    first commit

---
+++ start-git.sh
@@ -0,0 +1,14 @@
+# or create a new repository on the command line
+
+echo "# linux-commands-I-use-most" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/jhonatasfender/linux-commands-I-use-most.git
+git push -u origin master
+
+
+# or push an existing repository from the command line
+
+git remote add origin https://github.com/jhonatasfender/linux-commands-I-use-most.git
+git push -u origin mastergi 
