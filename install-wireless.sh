fc5f3669a15c8baf21a2731a3fe6bb5d7becd714
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2016-10-03 21:44:14 -0500

    install wireless

---
+++ install-wireless.sh
@@ -0,0 +1,15 @@
+
+lspci
+
+# Add a "non-free" component to /etc/apt/sources.list, for example:
+
+# Debian 8 "Jessie"
+deb http://httpredir.debian.org/debian/ jessie main contrib non-free
+
+# Update the list of available packages and install the firmware-ralink package:
+
+apt-get update 
+
+apt-get install firmware-ralink
+
+apt-get update 
