c960ae5ab54e134c53294368e2470f14b7b9903d
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2017-11-13 13:28:35 -0200

    Update and rename ionic-installer.sh to ionic.sh

---
+++ ionic.sh
@@ -0,0 +1,5 @@
+export ANDROID_HOME=/home/jonatas/Android/Sdk
+
+export PATH=$PATH:$ANDROID_HOME/add-ons:$ANDROID_HOME/emulator:$ANDROID_HOME/ndk-bundle:$ANDROID_HOME/platforms:$ANDROID_HOME/skins:$ANDROID_HOME/system-images:$ANDROID_HOME/build-tools:$ANDROID_HOME/extras:$ANDROID_HOME/licenses:$ANDROID_HOME/patcher:$ANDROID_HOME/platform-tools:$ANDROID_HOME/sources:$ANDROID_HOME/tools
+
+echo $PATH
